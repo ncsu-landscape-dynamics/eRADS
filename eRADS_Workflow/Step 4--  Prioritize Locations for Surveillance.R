d35ba957e902f6c55ca3a2643915b9c4c4e23a72
Wanwan <wliang@vols.utk.edu>
2020-05-03 23:12:18 -0400

    Create Step 4--  Prioritize Locations for Surveillance.R

---
+++ eRADS_Workflow/Step 4--  Prioritize Locations for Surveillance.R
@@ -0,0 +1,320 @@
+########################## 4. Prioritize Locations for Surveillance #############################
+## Detection probability ##
+p_dtc = function(surv_den,p_den,sensi){
+  p_dtc=1-(1-sensi)^(surv_den*p_den)
+  return(p_dtc)
+}
+
+## Surveillance density ##
+surv_den = function(confi,p_den,sensi){
+  #surv_den=log(1-confi, base = exp(1))/log(1-p_den*sens, base = exp(1))
+  p_den[p_den==0]=NA
+  surv_den=log(1-confi)/(p_den*log(1-sensi))
+  
+  return(surv_den)
+}
+
+host2=ra2plyMerge(host)
+host2bf=buffer(host2,width=2000)
+host2bf=gUnionCascaded(host2bf)
+
+#### Method 1 -- Select all locations with same detection confidence ####
+den=surv_den(0.90,pden,0.1)
+sum(getValues(den),na.rm = T)/sum(getValues(pden/pden),na.rm=T)
+plot(den,main="Trap Density")
+plot(cty,add=T,border="blue")
+plot(host2bf,add=T,border="red")
+den2=crop(den,host2bf)
+den2=mask(den2,host2bf)
+sum(getValues(den2),na.rm = T)*77
+
+#### Method 2 -- Select desired % of locations with highest infestation ####
+percent_locat = function(p_den,percentage,confi,sensi){
+  
+  n_all=sum(getValues(p_den/p_den),na.rm=T)
+  
+  pts=rasterToPoints(p_den,fun=function(x){x>0},spatial = T)
+  
+  pts$avl=extract(p_den,pts)
+ 
+  seleN=round(n_all*percentage)
+  
+  pts2=pts[order(pts$avl,decreasing = T),]
+  
+  pts3=pts2[1:seleN,]
+  ra2=rasterize(pts3,p_den,field=1,background=0)
+  
+  p_selec=ra2*p_den
+  survDen=surv_den(confi,p_selec,sensi)
+  
+  survTT=sum(getValues(survDen),na.rm = T)
+  n=sum(getValues(p_den/p_den),na.rm=T)
+  survMden=survTT/n
+  
+  results=list(survDen,survTT,survMden)
+  
+  return(results)
+}
+
+results=percent_locat(pden,0.95,0.95,0.1)
+plot(results[[1]])
+plot(cty,add=T,border="blue")
+results[[2]]
+results[[3]]
+den=results[[1]]
+den2=crop(den,host2bf)
+den2=mask(den2,host2bf)
+sum(getValues(den2),na.rm = T)*77
+
+
+#### Method 3 -- Select desired % of populations with highest infested locations prioritized ####
+percent_pop = function(p_den,percentage,confi,sensi){
+  
+  nt=sum(getValues(p_den/p_den),na.rm=T)
+  
+  pts=rasterToPoints(p_den,fun=function(x){x>0},spatial = T)
+  
+  pts$avl=extract(p_den,pts)
+  ap=sum(getValues(p_den), na.rm=T)
+  ctp1=ap*percentage
+  
+  pts2=pts[order(pts$avl,decreasing = T),]
+  
+  pts2$V3=sapply(1:length(pts2),FUN=function(i)sum(pts2$avl[1:i]))
+  pts3=pts2[pts2$V3<=ctp1,]
+  
+  ra2=rasterize(pts3,p_den,field=1,background=0)
+  
+  p_selec=ra2*p_den
+  survDen=surv_den(confi,p_selec,sensi)
+  pixel_percent=length(pts3)/nt
+  
+  survTT=sum(getValues(survDen),na.rm = T)
+  survMden=survTT/nt
+  
+  results=list(p_selec,survDen,pixel_percent,survTT,survMden)
+  return(results)
+}
+
+results=percent_pop(pden,0.95,0.95,0.1)
+survD=results[[2]]
+results[[3]]
+results[[4]]
+results[[5]]
+plot(survD)
+plot(cty,add=T,border="blue")
+
+den=survD
+den2=crop(den,host2bf)
+den2=mask(den2,host2bf)
+sum(getValues(den2),na.rm = T)*77
+
+
+#### Method 4 -- Select locations with infestation higher than given threshold ####
+pop_thresh = function(p_den=pden,threshold,confi,sensi){
+  p_den2=reclassify(p_den,c(0,threshold,NA))
+  survden= surv_den(confi,p_den2,sensi)
+  
+  n1=sum(getValues(p_den/p_den),na.rm = T)
+  n2=sum(getValues(p_den2/p_den2),na.rm = T)
+  ratio=n2/n1
+  
+  survTT=sum(getValues(survden),na.rm = T)
+  survMden=survTT/n1
+  
+  results=list(ratio,survden,survTT,survMden)
+  return(results)
+}
+
+results2=pop_thresh(pden,0.5,0.95,0.1)
+results2[[1]]
+survD=results2[[2]]
+results2[[3]]
+results2[[4]]
+plot(survD)
+plot(cty,add=T,border="blue")
+
+den=survD
+den2=crop(den,host2bf)
+den2=mask(den2,host2bf)
+sum(getValues(den2),na.rm = T)*77
+
+#### Method 5 -- Prioritize locations based on (probability), (epidemic size), and (hazard),#### 
+##and select % locations ##
+no=list(as.matrix(inf*0))
+inf_Itensi=stack()
+inf_Risk=stack()
+
+for (i in 1:10){
+  
+  random_seed=i*2901+20
+  dataNo <- pops_model(random_seed = random_seed, 
+                       use_lethal_temperature = use_lethal_temperature, 
+                       lethal_temperature = lethal_temperature, 
+                       lethal_temperature_month = lethal_temperature_month,
+                       
+                       infected = infected_speci[[1]],
+                       susceptible = susceptible_speci[[1]],
+                       total_plants = total_pl[[1]],
+                       mortality_on = mortality_on,
+                       mortality_tracker = infected_speci[[1]]*0,
+                       mortality = infected_speci[[1]]*0,
+                       treatment_maps = no,
+                       
+                       treatment_dates = c("2019-03-01"),
+                       pesticide_duration=c(0),
+                       resistant = infected_speci[[1]]*0,
+                       weather = weather,
+                       temperature = temperature,
+                       weather_coefficient = wc,
+                       ew_res = ew_res, ns_res = ns_res, num_rows = num_rows, num_cols = num_cols,
+                       time_step = time_step, reproductive_rate = reproductive_rate[[2]],
+                       mortality_rate = mortality_rate, mortality_time_lag = mortality_time_lag,
+                       season_month_start = season_month_start, season_month_end = season_month_end,
+                       start_date = start_time, end_date = end_time,
+                       treatment_method = "all infected", 
+                       
+                       natural_kernel_type = natural_kernel_type[[1]], anthropogenic_kernel_type = anthropogenic_kernel_type[[1]], 
+                       use_anthropogenic_kernel = use_anthropogenic_kernel, percent_natural_dispersal = percent_natural_dispersal[[1]],
+                       natural_distance_scale = natural_distance_scale[[2]], anthropogenic_distance_scale = anthropogenic_distance_scale[[1]], 
+                       natural_dir = natural_dir[[1]], natural_kappa = natural_kappa[[1]],
+                       anthropogenic_dir = anthropogenic_dir[[1]], anthropogenic_kappa = anthropogenic_kappa[[1]],output_frequency = "year")
+  
+  infNo=dataNo$infected
+  infNo=as.data.frame(infNo)
+  infNo=as(infNo,"matrix")
+  infNo=raster(infNo,template=inf)
+  inf_Itensi=stack(inf_Itensi,infNo)
+  
+  infNo=reclassify(infNo,c(0,1,0))
+  infNo=reclassify(infNo,c(1,Inf,1))
+  inf_Risk=stack(inf_Risk,infNo)
+}
+
+inf_more=mean(inf_Itensi)
+inf_more=round(reclassify(inf_more,c(0,1,0)))
+risk=mean(inf_Risk)
+
+rank_selectLoca = function(inf,inf_more,Wcoef=Tcoef,dispersal_rate=360,reprodution_rate=20,Nstep=8,host,risk,percentage=p){
+  
+  ply0=ra2plyMerge(inf)
+  ply1=ra2plyMerge(inf_more)
+  
+  gdiff=gDifference(ply1,ply0)
+  infnew=crop(inf_more,gdiff)
+  infnew=mask(infnew,gdiff)
+  
+  ts=infnew/infnew
+  nt=sum(getValues(ts),na.rm=T)
+  
+  
+  new_ply=ra2plyMerge(infnew)
+  
+  rt=host_m6(infnew,dispersal_rate,reproduction_rate,Nstep,Wcoef,ply1,host)
+  
+  
+  pixelArea=xres(inf)*yres(inf)
+  prob1=extract(risk,rt,buffer=0,fun=sum,na.rm=T)
+  ply_Infsize=extract(host,rt,buffer=0,fun=sum,na.rm=T)
+  
+  
+  surv_rank1=prob1*ply_Infsize
+  surv_rank2=prob1*rt$Nhost
+  surv_rank3=prob1*(ply_Infsize+rt$Nhost)
+  
+  
+  rt$rank_Epidsize=surv_rank1
+  rt$rank_Hzd=surv_rank2
+  rt$rank_EpidsizeHzd=surv_rank3
+  
+  rank_Epidsize=rt[order(rt$rank_Epidsize,decreasing = T),]
+  rank_Hzd=rt[order(rt$rank_Hzd,decreasing = T),]
+  rank_EpidsizeHzd=rt[order(rt$rank_EpidsizeHzd,decreasing = T),]
+  
+  nselec=round(nt*p)
+  
+  rank_Epidsize=rank_Epidsize[1:nselec,]
+  rank_Hzd=rank_Hzd[1:nselec,]
+  rank_EpidsizeHzd=rank_EpidsizeHzd[1:nselec,]
+  
+  ls=list(rank_Epidsize,rank_Hzd,rank_EpidsizeHzd)
+  
+  return(ls)
+}
+
+ranks= rank_selectLoca(inf,inf_more,Wcoef = Tcoef,dispersal_rate = 360,reprodution_rate = 20,Nstep = 8,host,risk=risk)
+epidsize=ranks[[1]]
+hzd=ranks[[2]]
+epidhzd=ranks[[3]]
+
+ply=ra2plyMerge(inf)
+ra_ply=rasterToPolygons(inf,fun=function(x) x>0)
+infestation_potential=Thost(ra_ply,1000,ra_ply[1,],host)
+infestation_potential$epidsize=infestation_potential$kde*infestation_potential$kde*(1+infestation_potential$Thst)
+infestation_potential=infestation_potential[order(infestation_potential$epidsize,decreasing = T),]
+plot(inf)
+
+
+
+
+rank_selectPopu = function(inf,inf_more,Wcoef=Tcoef,dispersal_rate=360,reprodution_rate=20,Nstep=8,host,risk,percentage=p){
+  
+  ply0=ra2plyMerge(inf)
+  ply1=ra2plyMerge(inf_more)
+  
+  gdiff=gDifference(ply1,ply0)
+  infnew=crop(inf_more,gdiff)
+  infnew=mask(infnew,gdiff)
+  
+  ts=infnew/infnew
+  nt=sum(getValues(ts),na.rm=T)
+  
+  
+  new_ply=ra2plyMerge(infnew)
+  
+  rt=host_m6(infnew,dispersal_rate,reproduction_rate,Nstep,Wcoef,ply1,host)
+  
+  
+  pixelArea=xres(inf)*yres(inf)
+  prob1=extract(risk,rt,buffer=0,fun=sum,na.rm=T)
+  ply_Infsize=extract(host,rt,buffer=0,fun=sum,na.rm=T)
+  
+  
+  surv_rank1=prob1*ply_Infsize
+  surv_rank2=prob1*rt$Nhost
+  surv_rank3=prob1*(ply_Infsize+rt$Nhost)
+  
+  
+  rt$rank_Epidsize=surv_rank1
+  rt$rank_Hzd=surv_rank2
+  rt$rank_EpidsizeHzd=surv_rank3
+  
+  rt$infIntensity=unlist(extract(infnew,rt,na.rm=T))
+  
+  rank_Epidsize=rt[order(rt$rank_Epidsize,decreasing = T),]
+  rank_Hzd=rt[order(rt$rank_Hzd,decreasing = T),]
+  rank_EpidsizeHzd=rt[order(rt$rank_EpidsizeHzd,decreasing = T),]
+  
+  Popselec=sum(rt$infIntensity)*p
+  
+  avl2$V3=sapply(1:dim(avl2)[1],FUN=function(i)sum(avl2[1:i,1]))
+  
+  rank_Epidsize$Cumu_inf=sapply(1:length(rt),FUN= function(i)sum(rank_Epidsize$infIntensity[1:i]))
+  rank_Hzd$Cumu_inf=sapply(1:length(rt),FUN= function(i)sum(rank_Hzd$infIntensity[1:i]))
+  rank_EpidsizeHzd$Cumu_inf=sapply(1:length(rt),FUN= function(i)sum(rank_EpidsizeHzd$infIntensity[1:i]))
+  
+  rank_Epidsize=rank_Epidsize[rank_Epidsize$Cumu_inf<=Popselec,]
+  rank_Hzd=rank_Hzd[rank_Hzd$Cumu_inf<=Popselec,]
+  rank_EpidsizeHzd=rank_EpidsizeHzd[rank_EpidsizeHzd$Cumu_inf<=Popselec,]
+  
+  ls=list(rank_Epidsize,rank_Hzd,rank_EpidsizeHzd)
+  
+  return(ls)
+}
+
+ranks= rank_selectPopu(inf,inf_more,Wcoef = Tcoef,dispersal_rate = 360,reprodution_rate = 20,Nstep = 8,host,risk=risk)
+epidsize=ranks[[1]]
+hzd=ranks[[2]]
+epidhzd=ranks[[3]]
+
+
